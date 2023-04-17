0ef974b452201ab3b8ffd2bdb2e0c47f55aa2212
kebab000 <dldytpq7010@gmail.com>
2023-04-17 22:29:45 +0900

    2023.04.17

--- homework/homeworkJs/2023.04.15.js
+++ homework/homeworkJs/2023.04.15.js
@@ -2,12 +2,19 @@ let aa = 0
 const dataQuestion = () => {
     const random = Math.floor( Math.random()*30)
     aa = random 
+
     fetch("../homeworkJson/dummy01.json")
     .then(res => res.json())
     .then(items => {
-        document.querySelector(".ddd .p").innerHTML=items.quotes[aa].quote
-        document.querySelector(".ddd .q").innerHTML=items.quotes[aa].author
+        let BGI = "url('https://source.unsplash.com/random/?star')"
+        document.querySelector(".ddd .p").innerHTML=items.quotes[random].quote
+        document.querySelector(".ddd .q").innerHTML=items.quotes[random].author
+        document.querySelector(".wrap").style.backgroundImage = BGI
+        
+        // setTimeout(function(){
+        //     location.reload();
+        // },3000);
     });
 };
 dataQuestion()
-setInterval(dataQuestion,10000)
+setInterval(dataQuestion,3000)
---
+++ homework/homeworkJs/2023.04.17.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector(".slider__inner");
+let index = 0
+let aa = 0
+let bb = 0
+function func (){
+    aa = (index += 1) % 3
+    slider.style.transform = "translateY(" + -810 * aa + "px)";
+    slider.style.transition = "all 0.6s";
+}
+const dataQuestion = () => {
+    const random = Math.floor( Math.random()*30);
+    const random1 = Math.floor( Math.random()*30000);
+    const random2 = Math.floor( Math.random()*30000);
+    bb = random 
+
+    fetch("../homeworkJson/dummy01.json")
+    .then(res => res.json())
+    .then(items => {
+        document.querySelector(".ddd .p").innerHTML=items.quotes[bb].quote
+        document.querySelector(".ddd .q").innerHTML=items.quotes[bb].author
+        if(aa == 1){
+            document.querySelector(".slider.s1 img").src = `https://source.unsplash.com/random/?city&t=${new Date().getTime()}`;
+            document.querySelector(".slider.s3 img").src = `https://source.unsplash.com/random/?forest&t=${random2}`;
+        } else if (aa == 2) {
+            document.querySelector(".slider.s1 img").src = `https://source.unsplash.com/random/?city&t=${new Date().getTime()}`;
+            document.querySelector(".slider.s2 img").src = `https://source.unsplash.com/random/?star&t=${random1}`;
+        } else {
+            document.querySelector(".slider.s2 img").src = `https://source.unsplash.com/random/?star&t=${random1}`;
+            document.querySelector(".slider.s3 img").src = `https://source.unsplash.com/random/?forest&t=${random2}`;
+
+        }
+    });
+};
+dataQuestion()
+setInterval(func,5000);
+setInterval(dataQuestion,5000)
