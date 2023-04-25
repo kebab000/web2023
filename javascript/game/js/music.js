20364458937397ca6049b13d98168fa02340e610
kebab000 <dldytpq7010@gmail.com>
2023-04-25 20:35:16 +0900

    2023.04.25

---
+++ javascript/game/js/music.js
@@ -0,0 +1,53 @@
+const allMusic = [
+    {
+        name : "1.가라가라",
+        artist : "dd",
+        img: "music_view01",
+        audio: "music_audio01"
+    }, {
+        name : "2.가라가라",
+        artist : "dd",
+        img: "music_view02",
+        audio: "music_audio02"
+    }, {
+        name : "3.가라가라",
+        artist : "dd",
+        img: "music_view03",
+        audio: "music_audio03"
+    }, {
+        name : "4.가라가라",
+        artist : "dd",
+        img: "music_view04",
+        audio: "music_audio04"
+    }, {
+        name : "5.가라가라",
+        artist : "dd",
+        img: "music_view05",
+        audio: "music_audio05"
+    }, {
+        name : "6.가라가라",
+        artist : "dd",
+        img: "music_view06",
+        audio: "music_audio06"
+    }, {
+        name : "7.가라가라",
+        artist : "dd",
+        img: "music_view07",
+        audio: "music_audio07"
+    }, {
+        name : "8.가라가라",
+        artist : "dd",
+        img: "music_view08",
+        audio: "music_audio08"
+    }, {
+        name : "9.가라가라",
+        artist : "dd",
+        img: "music_view09",
+        audio: "music_audio09"
+    }, {
+        name : "10.가라가라",
+        artist : "dd",
+        img: "music_view10",
+        audio: "music_audio10"
+    },
+]
